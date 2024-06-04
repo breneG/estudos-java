d8ec8f9f353e715dcaded6031a099308d931f702
Brener Gomes <brenergomes11@hotmail.com>
2024-06-04 18:47:25 -0300

    EstudoLista.v2-OrdenacaoPessoa

--- estudo-listas/src/Teste.java
+++ estudo-listas/src/Teste.java
@@ -1,21 +1,32 @@
+import Odenacao.OrdenacacoPessoa;
 import OperacoesBasicas.*;
 import Pesquisa.*;
 
 public class Teste {
     public static void main(String[] args) {
-        // TESTE CATALOGO DE LIVROS
-        CatalogoLivros catalogoLivros = new CatalogoLivros();
-        catalogoLivros.adicionarLivros("Livro 1", "Autor 1", 2010);
-        catalogoLivros.adicionarLivros("Livro 1", "Autor 2", 2021);
-        catalogoLivros.adicionarLivros("Livro 3", "Autor 2", 2022);
-        catalogoLivros.adicionarLivros("Livro 4", "Autor 3", 2023);
-        catalogoLivros.adicionarLivros("Livro 5", "Autor 4", 1994);
+        OrdenacacoPessoa ordenacacoPessoa = new OrdenacacoPessoa();
+        ordenacacoPessoa.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacacoPessoa.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacacoPessoa.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacacoPessoa.adicionarPessoa("Nome 4", 17, 1.56);
 
-        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
-        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2010, 2022));
-        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+        System.out.println("Ordenação por Altura:\n" + ordenacacoPessoa.ordenarPorAltura());
+        System.out.println();
+        System.out.println("Ordenação por Idade:\n" + ordenacacoPessoa.ordenarPorIdade());
 
-        // TESTE LISTA DE TAREFAS
+        // TESTE CATALOGO DE LIVROS - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // CatalogoLivros catalogoLivros = new CatalogoLivros();
+        // catalogoLivros.adicionarLivros("Livro 1", "Autor 1", 2010);
+        // catalogoLivros.adicionarLivros("Livro 1", "Autor 2", 2021);
+        // catalogoLivros.adicionarLivros("Livro 3", "Autor 2", 2022);
+        // catalogoLivros.adicionarLivros("Livro 4", "Autor 3", 2023);
+        // catalogoLivros.adicionarLivros("Livro 5", "Autor 4", 1994);
+
+        // System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        // System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2010, 2022));
+        // System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+
+        // TESTE LISTA DE TAREFAS - - - - - - - - - - - - - - - - - - - - - - - - - - -
         // ListaTarefas listaTarefa = new ListaTarefas();
 
         // System.out.println("O número total de elementos na lista é: " +
