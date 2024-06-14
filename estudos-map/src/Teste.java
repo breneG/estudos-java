207863adcee28bac51797bfad41ef8a4fa196493
Brener Gomes <brenergomes11@hotmail.com>
2024-06-13 22:51:49 -0300

    EstudoMap.v3-OrdenacaoMap

--- estudos-map/src/Teste.java
+++ estudos-map/src/Teste.java
@@ -1,4 +1,8 @@
+import java.time.LocalDate;
+import java.time.Month;
+
 import OperacoesBasicas.AgendaContatos;
+import OrdenacaoMap.AgendaEventos;
 import PesquisaMap.EstoqueProdutos;
 
 public class Teste {
@@ -20,17 +24,29 @@ public class Teste {
         // System.out.println("O número é: " + agendaContatos.pesquisarPorNome("Camila
         // DIO"));
 
-        EstoqueProdutos estoque = new EstoqueProdutos();
-        estoque.exibirProduto();
+        // EstoqueProdutos estoque = new EstoqueProdutos();
+        // estoque.exibirProduto();
 
-        estoque.adicionarProdutos(1L, "Produto A", 5.0, 10);
-        estoque.adicionarProdutos(2L, "Produto B", 10.0, 5);
-        estoque.adicionarProdutos(3L, "Produto C", 15.0, 2);
+        // estoque.adicionarProdutos(1L, "Produto A", 5.0, 10);
+        // estoque.adicionarProdutos(2L, "Produto B", 10.0, 5);
+        // estoque.adicionarProdutos(3L, "Produto C", 15.0, 2);
 
-        estoque.exibirProduto();
+        // estoque.exibirProduto();
 
-        System.out.println("Valor total do estoque: R$ " + estoque.calculaValorTotalEstoque());
-        System.out.println("Produto mais caro: \n" + estoque.obterProdutoMasiCaro());
+        // System.out.println("Valor total do estoque: R$ " +
+        // estoque.calculaValorTotalEstoque());
+        // System.out.println("Produto mais caro: \n" + estoque.obterProdutoMasiCaro());
 
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEventos(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEventos(LocalDate.of(2022, 7, 9), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEventos(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atração 3");
+        agendaEventos.adicionarEventos(LocalDate.of(2024, Month.JULY, 12), "Evento 4", "Atração 4");
+        agendaEventos.adicionarEventos(LocalDate.of(2024, Month.SEPTEMBER, 20), "Evento 5", "Atração 5");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
     }
 }
